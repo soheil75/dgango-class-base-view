83d742bd58bfed13de6894f0cf1e9f80f85d5a8f
soheil75 <soheilabdollahi1375@yahoo.com>
2021-03-28 19:03:17 +0430

    TemplateView

--- A/A/first/views.py
+++ A/A/first/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.views import View
+#from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Todo
 
-class Home(View):
+# class Home(View):
+#     template_name = "first/home.html"
+#     context = {'name':'Soheil'}
+#     def get(self,request,*args,**kwargs):
+#         return render (request,self.template_name,self.context)
+
+class Home(TemplateView):
     template_name = "first/home.html"
-    context = {'name':'Soheil'}
-    def get(self,request,*args,**kwargs):
-        return render (request,self.template_name,self.context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todos"] = Todo.objects.all()
+        return context
+    
+
+
+
