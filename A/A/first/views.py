1687ac495073ed01498807d54a4e63365957c1d4
soheil75 <soheilabdollahi1375@yahoo.com>
2021-03-29 14:31:51 +0430

    CreateView

--- A/A/first/views.py
+++ A/A/first/views.py
@@ -14,6 +14,7 @@ from django.urls import reverse_lazy
 from django.utils.text import slugify
 from django.contrib import messages
 
+from django.views.generic import CreateView
 
 # class Home(View):
 #     template_name = "first/home.html"
@@ -54,17 +55,29 @@ class DetailTodo(DetailView):
             return Todo.objects.none()
 
 
-class TodoCreate(FormView):
-    form_class = TodoCreateForm
+# class TodoCreate(FormView):
+#     form_class = TodoCreateForm
+#     template_name = 'first/todo_create.html'
+#     success_url = reverse_lazy('first:home')
+
+#     def form_valid(self,form):
+#         self.create_todo(form.cleaned_data)
+#         return super().form_valid(form)
+
+#     def create_todo(self,data):
+#         todo = Todo(title=data['title'],slug=slugify(data['title']))
+#         todo.save()
+#         messages.success(self.request,'your todo saved','success')
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = ('title',)
     template_name = 'first/todo_create.html'
     success_url = reverse_lazy('first:home')
 
-    def form_valid(self,form):
-        self.create_todo(form.cleaned_data)
-        return super().form_valid(form)
-
-    def create_todo(self,data):
-        todo = Todo(title=data['title'],slug=slugify(data['title']))
+    def form_valid(self, form):
+        todo = form.save(commit=False)
+        todo.slug = slugify(form.cleaned_data['title'])
         todo.save()
         messages.success(self.request,'your todo saved','success')
-
+        return super().form_valid(form)
